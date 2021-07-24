3e24a42a347243453c291c507d1025c5cf862a3c
Apit <apit.aprida@geekseat.com.au>
2021-07-24 23:01:26 +0700

    Providing static variable for a color palette.

--- Tests/AppsCoreTests/AppsCoreTests.swift
+++ Tests/AppsCoreTests/AppsCoreTests.swift
@@ -7,8 +7,14 @@ final class AppsCoreTests: XCTestCase {
         let color = AppsCore.colorFromHexString("FF0000")
         XCTAssertEqual(color, .red)
     }
+    
+    func testAppsColorsAreEquals() {
+        let color = AppsCore.colorFromHexString("006736")
+        XCTAssertEqual(color, AppsCore.appsColor)
+    }
 
     static var allTests = [
-        ("testColorRedEqual", testColorRedEqual)
+        ("testColorRedEqual", testColorRedEqual),
+        ("testAppsColorsAreEquals", testAppsColorsAreEquals)
     ]
 }
