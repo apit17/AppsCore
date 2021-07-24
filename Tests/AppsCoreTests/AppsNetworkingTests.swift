827d24a74b24e81b89825f90108e0cbb19057d14
Apit <apit.aprida@geekseat.com.au>
2021-07-25 00:45:35 +0700

    Refactored networking API to include dependency injection for easier testing

--- Tests/AppsCoreTests/AppsNetworkingTests.swift
+++ Tests/AppsCoreTests/AppsNetworkingTests.swift
@@ -6,16 +6,27 @@
 //
 
 import XCTest
-import AppsCore
+@testable import AppsCore
+
+class NetworkingSessionMock: NetworkSession {
+  var data: Data?
+  var error: Error?
+  
+  func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+    completionHandler(data, error)
+  }
+}
 
 final class AppsNetworkingTests: XCTestCase {
   
   func testLoadDataCall() {
     let manager = AppsCore.Networking.Manager()
+    let session = NetworkingSessionMock()
+    manager.session = session
     let expectation = XCTestExpectation(description: "Called for data")
-    guard let url = URL(string: "https://apple.com") else {
-      return XCTFail("Could not create URL properly")
-    }
+    let data = Data([1, 2, 3, 3, 1])
+    session.data = data
+    let url = URL(fileURLWithPath: "url")
     manager.loadData(from: url) { result in
       expectation.fulfill()
       switch result {
